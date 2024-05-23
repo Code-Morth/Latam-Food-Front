76d7aace48469303e268d3bb3d3003236748fe27
Droid-Jr <droidjr10@gmail.com>
2024-05-22 22:09:37 -0500

    agregado de medidas de responsi y carpetas de proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      phone: '414px',
+      phonelg: '568px',
+      tablet: '768px',
+      tabletlg: '960px',
+      tabletxl: '1024px',
+      laptop: '1200px',
+      laptoplg: '1400px',
+      desktop: '1700px',
+    },
   },
   plugins: [],
 }

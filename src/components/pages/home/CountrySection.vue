420b43ccfde41506a8a6e996cb1c642cfecb83c6
CodeMorth <kevinsalinasvd@gmail.com>
2024-06-03 16:38:32 -0500

    section flags

---
+++ src/components/pages/home/CountrySection.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import ButtonsGlobal from '../../atoms/ButtonsGlobal.vue';
+
+
+</script>
+
+<template>
+    <section class='CountrySection'>
+        <div class='country-section-container'>
+            <h1>Unimos lo mejor</h1>
+            <div class='flags-container'>
+                <div class='flag-text-container'>
+                    <div class='image-container'>
+                        <img src="/MexicoFlag.svg" alt="MexicoFlag">
+                    </div>
+                    <h2>México</h2>
+                    <p>Conocida por sus sabores vibrantes y variados. Sus platillos son emblemáticos y resultan
+                        aclamados por el mundo.</p>
+
+                </div>
+                <div class='flag-text-container'>
+                    <div class='image-container'>
+                        <img src="/ArgentinaFlag.svg" alt="ArgentinaFlag">
+                    </div>
+                    <h2>México</h2>
+                    <p>Conocida por sus sabores vibrantes y variados. Sus platillos son emblemáticos y resultan
+                        aclamados por el mundo.</p>
+
+                </div>
+                <div class='flag-text-container'>
+                    <div class='image-container'>
+                        <img src="/ColombiaFlag.svg" alt="ColombiaFlag">
+                    </div>
+                    <h2>México</h2>
+                    <p>Conocida por sus sabores vibrantes y variados. Sus platillos son emblemáticos y resultan
+                        aclamados por el mundo.</p>
+
+                </div>
+            </div>
+            <ButtonsGlobal class='saber-mas' >Saber Más</ButtonsGlobal>
+        </div>
+    </section>
+</template>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,14 +2,16 @@
 import ContainerComponents from "../../components/global/ContainerComponents.vue";
 import InformativeHome from "../../components/organins/home/InformativeHome.vue";
 import OurRecommendations from "../../components/organins/home/OurRecommendations.vue";
+import CountrySection from '../../components/pages/home/CountrySection.vue';
 
 </script>
 
 <template>
   <div class="w-full h-auto main-page">
     <InformativeHome />
-    <ContainerComponents> 
-        <OurRecommendations/>        
+    <ContainerComponents>
+      <OurRecommendations />
+      <CountrySection />
     </ContainerComponents>
   </div>
 </template>

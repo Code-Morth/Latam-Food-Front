3331e78a0c966c971150a2b64c5aae9d797d67f3
CodeMorth <kevinsalinasvd@gmail.com>
2024-06-05 00:03:15 -0500

    config prime react

--- src/components/organins/home/NewDishesAndPeople.vue
+++ src/components/organins/home/NewDishesAndPeople.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
+import ColorPicker from 'primevue/colorpicker';
 import CardTestimonials from '../../molecules/CardTestimonials.vue';
 
+import { ref } from "vue";
+
+const color = ref();
 
 </script>
 
 <template>
+    
     <section class='NewDisheAndPeople'>
+        
         <div class='new-dishes-and-people-container'>
             <header>
                 <h2>
                     Nuevos platos
 
                 </h2>
+                
                 <h2>Todas las semanas</h2>
             </header>
             <main>
                 <h3>Lo que dice la gente</h3>
+                <div class="card flex justify-content-center">
+                    <ColorPicker v-model="color" />
+                </div>
                 <p>Testimonios</p>
                 <div class='cards-container'>
                     <CardTestimonials />
@@ -25,5 +35,6 @@ import CardTestimonials from '../../molecules/CardTestimonials.vue';
                 </div>
             </main>
         </div>
+
     </section>
 </template>
--- src/pages/main.js
+++ src/pages/main.js
@@ -2,13 +2,15 @@ import { createApp } from 'vue';
 import '../styles/global.css';
 import App from './App.vue';
 import router from './router';
-
-// import Antd from 'ant-design-vue';
-// import 'ant-design-vue/dist/antd.css';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primevue/resources/primevue.min.css';   
+import 'primeicons/primeicons.css';  
 
 const app = createApp(App);
+app.use(PrimeVue);
 
 app.use(router);
-// app.use(Antd);
+
 
 app.mount('#app');

3f03920a9dce66b0e3ddc742b46db1c3a38c163d
CodeMorth <kevinsalinasvd@gmail.com>
2024-06-04 19:37:53 -0500

    comment main.js

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,12 +3,12 @@ import '../styles/global.css';
 import App from './App.vue';
 import router from './router';
 
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+// import Antd from 'ant-design-vue';
+// import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App);
 
 app.use(router);
-app.use(Antd);
+// app.use(Antd);
 
 app.mount('#app');

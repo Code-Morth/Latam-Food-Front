e1c507957df1f7a59415f562dec66bf129e8eb06
Droid-Jr <droidjr10@gmail.com>
2024-06-06 18:04:11 -0500

    loader

---
+++ src/components/loader/Loader.vue
@@ -0,0 +1,51 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+
+const completed = ref<any>(false)
+
+setTimeout(() => {
+    completed.value = true
+}, 2000);
+
+</script>
+
+<template>
+   <div class="main_box_loader">
+    <div class="border-[#bbecd8] h-[15rem] w-[15rem] animate-spin rounded-full border-8 border-t-[#29ca8a] relative z-20 " >
+        
+        </div>
+    <img v-if="completed" src="/components/contact/completed.png" alt="completado" class="absolute top-[40rem] rotate-[-5deg] w-[6rem] h-[3.5rem] main-page">
+
+    <div v-if="completed" class="flex flex-col justify-center items-center  gap-[.5rem]">
+        <h2 > ¡Compra exitosa!</h2>
+    <p >En breve tendrás tu pedido</p>
+    </div>
+    
+    <div v-else class="flex flex-col justify-center items-center gap-[.5rem]">
+        <h2 >Aguarde un momento</h2>
+        <p >Estamos procesando tu pedido</p>
+    </div>
+    
+   </div>
+</template>
+
+
+<style lang="postcss" scoped>
+
+.main_box_loader{
+    @apply w-full h-full flex flex-col gap-[.4rem] justify-center items-center fixed top-[5%]  ;
+    & h2 {
+        @apply text-[3rem] text-[black] font-bold;
+        @screen laptop{
+            font-size: calc(4.5rem * var(--scale));
+        } 
+    }
+    & p {
+        @apply text-[#515151] font-semibold  text-[1.5rem];
+        @screen laptop{
+            font-size: calc(2.5rem * var(--scale));
+        } 
+    }
+}
+
+</style>
--- src/pages/Contacto/Contacto.vue
+++ src/pages/Contacto/Contacto.vue
@@ -24,38 +24,49 @@ const cities = ref([
       <div class="box_main_cont">
 
         <div class="box_content_2 ">¨¨
-          <div class="flex flex-col gap-[1.5rem]">
+          <div class="main_____">
             <div class="content_title ">
               <h4>Estemos en</h4>
               <h3>contacto</h3>
             </div>
             <p>Te proporcionamos nuestras vías de contacto en caso de que necesites una atención más personalizada.</p>
-            
+
           </div>
 
           <form class="forma_box ">
             <input type="text" placeholder="Nombre *">
             <input type="email" placeholder="Email *">
             <input type="email" placeholder="Numero de teléfono *">
-            <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="¿Cómo nos encontraste?" class="w-full py-[1rem]" />
+            <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="¿Cómo nos encontraste?"
+              class="w-full py-[1rem]" />
             <button>Enviar</button>
           </form>
 
-          <div>
-            <h4>Telefono</h4>
-            <div>
-              <h5>Celular</h5>
-              <h5>03 5432 1234</h5>
+
+          <div class="content_main_box_box ">
+
+            <div class="flex items-center gap-[1rem]">
+              <img src="/components/contact/fono.png" alt="fono">
+              <div class="content_iconos_">
+                <h5>Celular</h5>
+                <h6>03 5432 1234</h6>
+              </div>
             </div>
+
+
+            <div class="flex items-center gap-[1rem]">
+              <img src="/components/contact/email.png" alt="fono">
+              <div class="content_iconos_">
+                <h5>Email</h5>
+                <h6>info@marcc.com.au</h6>
+              </div>
+            </div>
+
           </div>
 
-          <div>
-            <h4>Email</h4>
-            <div>
-              <h5>Email</h5>
-              <h5>info@marcc.com.au</h5>
-            </div>
-          </div>
+
+
+
         </div>
         <div class="w-full h-[70rem]">
           <iframe class="w-full h-full"
@@ -63,8 +74,9 @@ const cities = ref([
             width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-
       </div>
+
+
 
     </ContainerComponents>
   </div>
@@ -72,21 +84,65 @@ const cities = ref([
 
 
 <style lang="postcss" scoped>
+.main_____ {
+  @apply flex flex-col gap-[3rem];
+
+  @screen tablet {
+    @apply gap-[2rem];
+  }
+
+  @screen laptop {
+    @apply gap-[2rem];
+  }
+}
+
+.content_main_box_box {
+  @apply flex gap-[3rem];
+
+  @screen tablet {
+    @apply gap-[10rem];
+  }
+
+  @screen laptop {
+    gap: calc(15rem * var(--scale))
+  }
+}
+
+.content_iconos_ {
+  @apply flex flex-col gap-[.2rem];
+
+  & h5 {
+    @apply font-bold text-[1.5rem];
+  }
+
+  & h6 {
+    @apply text-[#F08A1D] font-bold text-[1.4rem];
+  }
+
+
+}
+
+img {
+  @apply w-[4rem] h-[3.5rem];
+}
+
+
+
 .box_main_cont {
   @apply flex flex-col gap-[7rem];
 
-  @screen tablet {
+  @screen laptop {
     @apply flex-row;
   }
 }
 
 ::placeholder {
-  @apply text-[#313131] font-bold text-[1.3rem] ;
+  @apply text-[#313131] font-bold text-[1.3rem];
 }
 
 
 .box_content_2 {
-  @apply w-full h-[70rem] bg-[#00000046] flex flex-col gap-[1rem];
+  @apply w-full h-[70rem] flex flex-col gap-[2rem];
 
   & p {
     @apply text-[#000000] text-[1.5rem] w-[75%];
@@ -99,23 +155,49 @@ const cities = ref([
 
 
 .forma_box {
-  @apply flex flex-col gap-[2.5rem] w-[60%] h-auto;
+  @apply flex flex-col gap-[2.5rem] w-[90%] h-auto my-[2rem];
+
+  @screen tablet {
+    @apply w-[90%];
+  }
+
+  @screen laptop {
+    @apply w-[73%];
+  }
 
   & input {
-    @apply py-[1.5rem] px-[2rem];
+    @apply py-[1.5rem] px-[2rem] border-[#E0E0E0] border;
+
   }
- 
+
+  input:focus {
+    @apply border-[#fda74b] outline-none;
+  }
+
+  & button {
+    @apply w-full py-[1rem] bg-[#F7A043] rounded-md text-[white] font-semibold text-[2rem] duration-300 ease-in-out hover:bg-[#ffae58e8];
+
+    @screen laptop {
+      font-size: calc(2.5rem * var(--scale));
+    }
+  }
+
 }
 
-.p-dropdown:not(.p-disabled).p-focus{
-  @apply outline-[#F7A043] outline-[.2rem] ;
+.p-dropdown:not(.p-disabled).p-focus {
+  @apply outline-[#F7A043] outline-[.2rem];
 }
-.p-dropdown-label:hover{
-  @apply bg-[red] ;
+
+.p-dropdown {
+  @apply rounded-[.3rem] border-[#E0E0E0] border-[.1rem];
 }
 
 .content_title {
-  @apply flex flex-col gap-[2rem];
+  @apply flex flex-col;
+
+  @screen laptop {
+    @apply gap-[2.4rem];
+  }
 
   & h4 {
     @apply font-bold text-[6rem];
@@ -128,8 +210,12 @@ const cities = ref([
   & h3 {
     @apply font-bold text-[4rem] text-[#F7A043];
 
+    @screen tablet {
+      @apply text-[6rem];
+    }
+
     @screen laptop {
-      font-size: calc(4.5rem * var(--scale));
+      font-size: calc(6rem * var(--scale));
     }
   }
 }
